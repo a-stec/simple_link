a14867a4a4aa55c69d6fa73b52c0fd42f9ed5cd1
AndrejStece <stecenko_andrej@mail.ru>
2023-07-25 13:30:36 +0500

    positioning of elements

--- index.php
+++ index.php
@@ -5,18 +5,34 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Simple.Link</title>
+  <link rel="stylesheet" href="./assets/css/normalize.css">
   <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
-<body>
+<body class="body">
   <header class="header">
-    <h1></h1>
-  </header>
-  <section class="shortener">
-    <div class="container">
-
+    <div class="container flex header__container">
+      <h1 class="header__title">Simple.Link</h1>
+      <strong class="header__slogan">Сервис сокращения ссылок</strong>
     </div>
-  </section>
+  </header>
+
+  <main class="main">
+    <div class="container">
+      <div class="alert hidden">
+        <strong>Запрос успешно выполнен</strong>
+      </div>
+      <form action="" class="form flex main__form">
+        <input type="text" class="input" placeholder="Введите URL-адрес" name="url">
+        <button class="btn btn-reset form__btn">Сократить</button>
+      </form>
+      <div class="result">
+        <input type="text" class="input" readonly>
+        <button class="btn btn-reset result__btn">Копировать</button>
+      </div>
+    </div>
+  </main>
+
 </body>
 
 </html>
