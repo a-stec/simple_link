daec284c8b0141543615625873b2d12930ebbcca
AndrejStece <stecenko_andrej@mail.ru>
2024-02-11 13:19:37 +0500

    edits 404

--- 404.php
+++ 404.php
@@ -3,9 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>404</title>
+  <link rel="stylesheet" href="./assets/css/normalize.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="./assets/js/script.js"></script>
 </head>
 <body>
-  ошибка
+  <div class="info">
+    404 | Not Found
+  </div>
 </body>
 </html>
